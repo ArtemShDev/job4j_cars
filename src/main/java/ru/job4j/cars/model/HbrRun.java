e2d73dce2ddeeedda87c5e8eeff1a5406b19e213
ArtemShDev <dev.art.balt@gmail.com>
2021-12-30 03:46:06 +0300

    Реализовать пользовательские фильтры для площадок машин [#4745]

--- src/main/java/ru/job4j/cars/model/HbrRun.java
+++ src/main/java/ru/job4j/cars/model/HbrRun.java
@@ -6,6 +6,8 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.Set;
+
 public class HbrRun {
 
     public static void main(String[] args) {
@@ -15,13 +17,16 @@ public class HbrRun {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
             Session session = sf.openSession();
             session.beginTransaction();
-            User user = new User("Vasya", "1@gmail.com", "123");
+            User user = new User("Vasya", "2@gmail.com", "123");
             Brand brand = new Brand("Toyota");
-            CarBody carBody = new CarBody("Universal");
+            CarBody carBody = new CarBody("Sedan");
             Photo photo1 = new Photo("./photos/1.png");
             Photo photo2 = new Photo("./photos/2.png");
             Photo photo3 = new Photo("./photos/3.png");
-            Ad ad = new Ad("Sale Toyota Avensis", "Sale Toyota Avensis 1.8 2009", user, carBody, brand);
+            Ad ad = new Ad("Sale Toyota Corolla", "Sale Toyota Corolla 1.8 2009", user, carBody, brand);
+            ad.addPhoto(photo1);
+            ad.addPhoto(photo2);
+            ad.addPhoto(photo3);
             session.save(ad);
             session.getTransaction().commit();
             session.close();
